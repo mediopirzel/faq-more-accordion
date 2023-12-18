bb6532fcb529b01e67affbbd2b87a66ed218905d
mediopirzel <xaviersoler@gmail.com>
2023-12-18 17:08:49 +0100

    Init repository

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'a078bfb8e89231388632');
---
+++ build/render.php
@@ -0,0 +1,3 @@
+<p <?php echo get_block_wrapper_attributes(); ?>>
+	<?php esc_html_e( 'Faq More Accordion – hello from a dynamic block!', 'faq-more-accordion' ); ?>
+</p>
